63d5e73023b1aa3d1746663077dce74e3ee66b03
vijaykumarrio <30952904+vijaykumarrio@users.noreply.github.com>
2017-09-01 10:15:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/Article Vijay', function (req,res){
+app.get('/Article- Vijay', function (req,res){
 res.send('This is  Article vijay');
 });
-app.get('/Article kumar', function (req,res){
+app.get('/Article -kumar', function (req,res){
 res.send('This is  Article kumar');
 });
-app.get('/Article Lion', function (req,res){
+app.get('/Article -Lion', function (req,res){
 res.send('This is  Article Lion');
 });
 app.get('/ui/style.css', function (req, res) {
