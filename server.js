e78454b573e23100b496856f085ae80bbc4b29f8
vijaykumarrio <30952904+vijaykumarrio@users.noreply.github.com>
2017-09-01 11:53:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.get('/Article-Lion', function (req,res){
 res.sendFile(path.join(__dirname, 'ui', 'Article-Lion.html'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
